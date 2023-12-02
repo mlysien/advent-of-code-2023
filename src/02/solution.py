a5aa0e6e61e3a5d7181e460d2aa1300fbc6157a6
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2023-12-02 20:19:52 +0100

    resolved first part

---
+++ src/02/solution.py
@@ -0,0 +1,65 @@
+import re
+
+
+def open_file():
+    file = open("puzzle.txt", "r")
+    return file.read().split('\n')
+
+
+def find_digit(txt):
+    return int(re.findall(r'\d+', txt)[0])
+
+
+class Set:
+    red: 0
+    green: 0
+    blue: 0
+
+    def __init__(self, red, green, blue):
+        self.red = red
+        self.green = green
+        self.blue = blue
+
+lines = open_file()
+game_id = 0
+games = []
+
+for line in lines:
+
+    game_sets = []
+    game_id += 1
+    sets_of_cubes = line.split(':')[1].split(';')
+
+    for sets in sets_of_cubes:
+        r = g = b = 0
+        for cube in sets.split(','):
+            if cube.__contains__('red'):
+                r = find_digit(cube)
+            if cube.__contains__('green'):
+                g = find_digit(cube)
+            if cube.__contains__('blue'):
+                b = find_digit(cube)
+        game_sets.append(Set(r, g, b))
+    games.append({game_id: game_sets})
+
+ids = []
+for game in games:
+    blues = greens = reds = 0
+    for v in game.values():
+        is_valid = True
+
+        for x in v:
+            if x.red > 12:
+                is_valid = False
+                break
+            if x.green > 13:
+                is_valid = False
+                break
+            if x.blue > 14:
+                is_valid = False
+                break
+        if is_valid:
+            ids.append(list(game.keys())[0])
+
+
+print(sum(ids))
